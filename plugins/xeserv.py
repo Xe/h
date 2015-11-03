739165cac24ab6c813e28e16bd390438a59f78f7
Christine Dodrill <xena@yolo-swag.com>
2015-11-02 17:28:49 -0800

    start xeserv.us plugin

---
+++ plugins/xeserv.py
@@ -0,0 +1,21 @@
+from util import hook
+
+import requests
+
+@hook.command
+def minecraft(pls):
+    r = requests.get("http://xeserv.us/api/minecraft.json")
+
+    data = r.json()
+
+    if not data["online"]:
+        return "fluttershy.yochat.biz is down, oh noes"
+
+    if data["players"] == None:
+        return "fluttershy.yochat.biz has no online players"
+
+    return "fluttershy.yochat.biz has the following players: " + (reduce (lambda x, y: x + " " + y), data["players"])
+
+@hook.command
+def tf2(pls):
+    pass
