f339bf56135314777b1e538ee5a0ab3e6c3bc89d
Christine Dodrill <xena@yolo-swag.com>
2015-11-04 17:11:00 -0800

    .source pull

--- plugins/source.py
+++ plugins/source.py
@@ -1,5 +1,10 @@
+import os
+
 from util import hook
 
 @hook.command
 def source(inp):
+    if inp == "pull":
+        os.system("git pull")
+        return "updating..."
     return "my source code: https://git.xeserv.us/xena/h"
