46ba0694bb67fb40c7f33675ed2e3b4dea1d9738
Christine Dodrill <xena@yolo-swag.com>
2015-11-09 12:38:53 -0800

    fix printer fact regex

--- plugins/printerfact.py
+++ plugins/printerfact.py
@@ -4,12 +4,13 @@ import requests, re, random
 
 regex = re.compile(re.escape("cat"), re.IGNORECASE)
 kittenrex = re.compile(re.escape("kitten"), re.IGNORECASE)
+preggorex = re.compile(re.escape("pregmant"), re.IGNORECASE)
 
 @hook.regex("PHP sadness$")
 def php_fact(inp):
     return "http://phpsadness.com/sad/" + str(random.randint(0,53))
 
-@hook.regex("printer fact$")
+@hook.regex("^printer fact$")
 @hook.command
 def printerfact(inp, say=None):
     r = requests.get('https://catfacts-api.appspot.com/api/facts?number=1')
